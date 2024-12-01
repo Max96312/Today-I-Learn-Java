148176fb40fdcf9b1b83e11128915a20feba5bf1
Max96312 <k0mp4ny16@gmail.com>
2024-12-01 14:03:35 +0900

    sec05: 래퍼, Class 클래스

--- Java/java-mid1/src/lang/system/SystemMain.java
+++ Java/java-mid1/src/lang/system/SystemMain.java
@@ -1,5 +1,7 @@
 package lang.system;
 
+import java.util.Arrays;
+
 public class SystemMain {
     public static void main(String[] args) {
         // 현재 시간(밀리초)를 가져온다.
@@ -21,5 +23,13 @@ public class SystemMain {
         char[] originalArray = {'h', 'e', 'l', 'l', 'o'};
         char[] copiedArray = new char[5];
         System.arraycopy(originalArray, 0, copiedArray, 0, originalArray.length);
+
+        // 배열 출력
+        System.out.println("copiedArray = " + copiedArray);
+        System.out.println("Arrays.toString = " + Arrays.toString(copiedArray));
+
+        // 프로그램 종료
+        System.exit(0);
+        System.out.println("hello");
     }
 }
