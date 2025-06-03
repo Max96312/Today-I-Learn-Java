ae4866f24626523629063df1ea6b601e2e7c4507
Max96312 <k0mp4ny16@gmail.com>
2025-06-03 12:35:27 +0900

    sec09: 생산자 소비자 문제1

--- Java/java-adv1/src/thread/bounded/BoundedMain.java
+++ Java/java-adv1/src/thread/bounded/BoundedMain.java
@@ -9,7 +9,8 @@ import static util.ThreadUtils.sleep;
 public class BoundedMain {
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
-        BoundedQueue queue = new BoundedQueueV1(2);
+        // BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
         // produceFirst(queue); // 생산자 먼저 실행
@@ -21,7 +22,7 @@ public class BoundedMain {
         List<Thread> threads = new ArrayList<>();
         startConsumer(queue, threads);
         printAllState(queue, threads);
-        startProdecer(queue, threads);
+        startProducer(queue, threads);
         printAllState(queue, threads);
         log("== [소비자 먼저 실행] 종료, " + queue.getClass().getSimpleName() + "==");
     }
@@ -29,7 +30,7 @@ public class BoundedMain {
     private static void produceFirst(BoundedQueue queue) {
         log("== [생산자 먼저 실행] 시작, " + queue.getClass().getSimpleName() + "==");
         List<Thread> threads = new ArrayList<>();
-        startProdecer(queue, threads);
+        startProducer(queue, threads);
         printAllState(queue, threads);
         startConsumer(queue, threads);
         printAllState(queue, threads);
@@ -55,7 +56,7 @@ public class BoundedMain {
         }
     }
 
-    private static void startProdecer(BoundedQueue queue, List<Thread> threads) {
+    private static void startProducer(BoundedQueue queue, List<Thread> threads) {
         System.out.println();
         log("생산자 시작");
         for (int i = 1; i <= 3; i++) {
