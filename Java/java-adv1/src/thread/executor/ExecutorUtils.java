d3eda7da66b86e3efc600747386c2669153fe4d1
Max96312 <k0mp4ny16@gmail.com>
2025-07-01 22:00:54 +0900

    sec14: 스레드 풀과 Executor 프레임워크2

--- Java/java-adv1/src/thread/executor/ExecutorUtils.java
+++ Java/java-adv1/src/thread/executor/ExecutorUtils.java
@@ -19,4 +19,18 @@ public abstract class ExecutorUtils {
             log(executorService);
         }
     }
+
+    public static void printState(ExecutorService executorService, String taskName) {
+        if(executorService instanceof ThreadPoolExecutor poolExecutor) {
+            // int pool = poolExecutor.getCorePoolSize(); // 설정된 고정 최소 쓰레드 수
+            int pool = poolExecutor.getPoolSize(); // 현재 풀에 살아 있는 전체 쓰레드 수
+            int active = poolExecutor.getActiveCount();
+            int queuedTasks = poolExecutor.getQueue().size();
+            long completedTask = poolExecutor.getCompletedTaskCount();
+            log(taskName + " -> [pool=" + pool + ", active=" + active + ", queuedTasks" + queuedTasks +
+                    ", completedTasks=" + completedTask + "]");
+        } else {
+            log(executorService);
+        }
+    }
 }
