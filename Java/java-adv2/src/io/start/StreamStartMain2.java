75933a9223d77fc8fcd5259f4aeb7e2cfefc243b
Max96312 <k0mp4ny16@gmail.com>
2025-07-05 15:18:19 +0900

    sec03: I/O 기본1

--- Java/java-adv2/src/io/start/StreamStartMain2.java
+++ Java/java-adv2/src/io/start/StreamStartMain2.java
@@ -1,11 +1,10 @@
 package io.start;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class StreamStartMain1 {
+public class StreamStartMain2 {
     public static void main(String[] args) throws IOException {
         FileOutputStream fos = new FileOutputStream("temp/hello.dat");
         fos.write(65);
@@ -14,10 +13,12 @@ public class StreamStartMain1 {
         fos.close();
 
         FileInputStream fis = new FileInputStream("temp/hello.dat");
-        System.out.println(fis.read());
-        System.out.println(fis.read());
-        System.out.println(fis.read());
-        System.out.println(fis.read());
+
+        int data;
+        while ((data = fis.read()) != -1) {
+            System.out.println(data);
+        }
+
         fis.close();
     }
 }
--- Java/java-adv2/src/io/start/StreamStartMain4.java
+++ Java/java-adv2/src/io/start/StreamStartMain4.java
@@ -1,23 +1,21 @@
+
 package io.start;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
-public class StreamStartMain1 {
+public class StreamStartMain4 {
     public static void main(String[] args) throws IOException {
         FileOutputStream fos = new FileOutputStream("temp/hello.dat");
-        fos.write(65);
-        fos.write(66);
-        fos.write(67);
+        byte[] input = {65, 66, 67};
+        fos.write(input);
         fos.close();
 
         FileInputStream fis = new FileInputStream("temp/hello.dat");
-        System.out.println(fis.read());
-        System.out.println(fis.read());
-        System.out.println(fis.read());
-        System.out.println(fis.read());
+        byte[] readBytes = fis.readAllBytes();
+        System.out.println(Arrays.toString(readBytes));
         fis.close();
     }
 }
