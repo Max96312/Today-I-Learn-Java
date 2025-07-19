562507e6486109caa077fef0a22fb03b21fa195d
Max96312 <k0mp4ny16@gmail.com>
2025-07-19 19:03:08 +0900

    sec08: 네트워크 - 프로그램1

--- Java/java-adv2/src/network/tcp/v2/ServerV2.java
+++ Java/java-adv2/src/network/tcp/v2/ServerV2.java
@@ -1,4 +1,4 @@
-package network.tcp.v1;
+package network.tcp.v2;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 import static util.MyLogger.log;
 
-public class ServerV1 {
+public class ServerV2 {
     private static final int PORT = 12345;
 
     public static void main(String[] args) throws IOException {
@@ -22,14 +22,20 @@ public class ServerV1 {
         DataInputStream input = new DataInputStream(socket.getInputStream());
         DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-        // 클라이언트로부터 문자 받기
-        String received = input.readUTF();
-        log("client -> server: " + received);
+        while (true) {
+            // 클라이언트로부터 문자 받기
+            String received = input.readUTF();
+            log("client -> server: " + received);
 
-        // 클라이언트에게 문자 보내기
-        String toSend = received + "World";
-        output.writeUTF(toSend);
-        log("client <- server: " + toSend);
+            if (received.equals("exit")) {
+                break;
+            }
+
+            // 클라이언트에게 문자 보내기
+            String toSend = received + "World";
+            output.writeUTF(toSend);
+            log("client <- server: " + toSend);
+        }
 
         // 자원 정리
         log("연결 종료: " + socket);
