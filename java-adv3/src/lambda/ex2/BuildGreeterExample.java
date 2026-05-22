cfa96ac783091f716d063603c7ac52a1fac14011
Max96312 <k0mp4ny16@gmail.com>
2026-05-22 23:19:02 +0900

    Sec02: 람다가 필요한 이유

---
+++ java-adv3/src/lambda/ex2/BuildGreeterExample.java
@@ -0,0 +1,12 @@
+package lambda.ex2;
+
+public class BuildGreeterExample {
+    // 고차 함수, greeting 문자열을 받아, "새로운 함수를" 반환
+    public static StringFunction buildGreeter(String greeting) {
+        // 코드 작성
+        return null; // 적절한 람다 반환
+    }
+    public static void main(String[] args) {
+        // 코드 작성
+    }
+}
