7aefea9d4061fab6ebd08031d21e26abd5ed087f
Max96312 <k0mp4ny16@gmail.com>
2026-06-12 23:16:08 +0900

    Sec05: 람다 활용

--- java-adv3/src/lambda/lambda5/mystrem/MyStreamV3.java
+++ java-adv3/src/lambda/lambda5/mystrem/MyStreamV3.java
@@ -2,42 +2,50 @@ package lambda.lambda5.mystrem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 // static factory 추가
 public class MyStreamV3<T> {
 
-    private List<Integer> internalList;
+    private List<T> internalList;
 
-    private MyStreamV3(List<Integer> internalList) {
+    private MyStreamV3(List<T> internalList) {
         this.internalList = internalList;
     }
 
     // static factory
-    public static MyStreamV3 of(List<Integer> internalList) {
+    public static <T> MyStreamV3<T> of(List<T> internalList) {
         return new MyStreamV3(internalList);
     }
 
-    public MyStreamV3 filter(Predicate<Integer> predicate) {
-        List<Integer> filtered  = new ArrayList<>();
-        for (Integer element : internalList) {
+    public MyStreamV3<T> filter(Predicate<T> predicate) {
+        List<T> filtered  = new ArrayList<>();
+        for (T element : internalList) {
             if (predicate.test(element)) {
                 filtered.add(element);
             }
         }
-        return new MyStreamV3(filtered);
+        return MyStreamV3.of(filtered);
     }
 
-    public MyStreamV3 map(Function<Integer, Integer> mapper) {
-        List<Integer> mapped = new ArrayList<>();
-        for (Integer element : internalList) {
+    public <R> MyStreamV3<R> map(Function<T, R> mapper) {
+        List<R> mapped = new ArrayList<>();
+        for (T element : internalList) {
             mapped.add(mapper.apply(element));
         }
-        return new MyStreamV3(mapped);
+        return MyStreamV3.of(mapped);
     }
 
-    public List<Integer> toList() {
+    public List<T> toList() {
         return internalList;
+    }
+
+    // 추가
+    public void foreach(Consumer<T> consumer) {
+        for (T element : internalList) {
+            consumer.accept(element);
+        }
     }
 }
