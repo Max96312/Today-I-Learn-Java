218fb2e4e4f68a0c7af9581e64dc4c6eb079b158
Max96312 <k0mp4ny16@gmail.com>
2026-08-05 23:26:29 +0900

    sec14. 애노테이션

---
+++ java-adv2/src/annotation/basic/inherited/InheritedMain.java
@@ -0,0 +1,19 @@
+package annotation.basic.inherited;
+
+import java.lang.annotation.Annotation;
+
+public class InheritedMain {
+
+    public static void main(String[] args) {
+        print(Parent.class);
+        print(Child.class);
+    }
+
+    private static void print(Class<?> clazz) {
+        System.out.println("clazz = " + clazz);
+        for (Annotation annotation : clazz.getAnnotations()) {
+            System.out.println(" - " + annotation.annotationType().getSimpleName());
+        }
+        System.out.println();
+    }
+}
