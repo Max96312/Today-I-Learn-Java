56948ff4453bef558e5ccd1748cc9f5e0b829f1e
Max96312 <k0mp4ny16@gmail.com>
2026-08-06 22:49:15 +0900

    sec14. 애노테이션

---
+++ java-adv2/src/annotation/java/OverrideMain.java
@@ -0,0 +1,22 @@
+package annotation.java;
+
+public class OverrideMain {
+
+    static class A {
+        public void call() {
+            System.out.println("A.call");
+        }
+    }
+
+    static class B extends A {
+//        @Override
+        public void callll() {
+            System.out.println("B.callll");
+        }
+    }
+
+    public static void main(String[] args) {
+        A a = new B();
+        a.call();
+    }
+}
