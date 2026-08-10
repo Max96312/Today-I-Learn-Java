e6d3cd1b79ba3ec444ab6e3986f433fe4c1c2a3f
Max96312 <k0mp4ny16@gmail.com>
2026-08-10 23:41:06 +0900

    sec15. HTTP 서버 활용

--- java-adv2/src/was/v8/ServerMainV8.java
+++ java-adv2/src/was/v8/ServerMainV8.java
@@ -5,6 +5,7 @@ import was.httpserver.ServletManager;
 import was.httpserver.servlet.DiscardServlet;
 import was.httpserver.servlet.annotation.AnnotationServletV1;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 import was.v7.SearchControllerV7;
 import was.v7.SiteControllerV7;
 
@@ -17,7 +18,8 @@ public class ServerMainV8 {
 
     public static void main(String[] args) throws IOException {
         List<Object> controllers = List.of(new SiteControllerV8(), new SearchControllerV8());
-        AnnotationServletV2 annotationServlet = new AnnotationServletV2(controllers);
+//        AnnotationServletV2 annotationServlet = new AnnotationServletV2(controllers);
+        AnnotationServletV3 annotationServlet = new AnnotationServletV3(controllers);
 
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(annotationServlet);
